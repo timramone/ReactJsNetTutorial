be610959b056b142dc045a0a210fd406f392afe3
timramone <timramone@gmail.com>
2014-11-08 18:31:32 +0300

    Template app

---
+++ ReactJsTutorial/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ReactJsTutorial
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
